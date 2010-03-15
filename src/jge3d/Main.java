2475273f49e97c02c8a8eaba5fc2b7795b120627
rnolan <rnolan@rnolan.semiconassociates.com>
2010-03-15 18:50:09 -0400

    Setting up the window.

--- src/jge3d/Main.java
+++ src/jge3d/Main.java
@@ -1,10 +1,23 @@
 package jge3d;
 
+import javax.swing.Timer;
+import jge3d.Window;
+
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("hello world");
-	}
+		//Create window
+		Window window = new Window();
+		
+		//Enter Main Loop
+		Timer timer = new Timer(0, null);
+		float oldTime = 0;
+		float newTime;
 
+		/*do{
+			//Get time
+			//newTime = timer.
+			
+			//Compare to last time
+		}while(true);*/
+	}
 }
